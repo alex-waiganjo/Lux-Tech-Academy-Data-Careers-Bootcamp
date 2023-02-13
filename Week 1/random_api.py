5b63654f8c1927a394c35923c1067137d0b13b94
alex-waiganjo <alexwaiganjo934@gmail.com>
2023-02-14 00:12:19 +0300

    week 1

---
+++ Week 1/random_api.py
@@ -0,0 +1,18 @@
+# Intermediate: 
+# Using Python and the appropriate Python package of your choice, access https://randomuser.me/ api and list the top 100 male users. 
+
+
+import requests 
+payload = {'gender': 'male','results':100}
+r = requests.get('https://randomuser.me/api',params=payload)
+
+print(r.url) #ouput the url https://randomuser.me/api?gender=male&results=100
+
+# Testing whether the status code is correct
+if r.status_code == 200:
+    result= r.json()
+    for p, user in enumerate(result["results"]):
+        print(f"{p + 1}. {user['name']['first']} {user['name']['last']}")
+else:
+    print("Sorry Inavalid Response")
+
