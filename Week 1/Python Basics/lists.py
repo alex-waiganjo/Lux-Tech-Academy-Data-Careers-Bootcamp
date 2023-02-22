fef9aeddad17f08837614e937976cf55f21b92c6
alex-waiganjo <alexwaiganjo934@gmail.com>
2023-02-23 00:08:49 +0300

    lists

--- Week 1/Python Basics/lists.py
+++ Week 1/Python Basics/lists.py
@@ -119,3 +119,49 @@ print(common_units) #['Programming', 'AI', 'Ml']
 #Using the Clear() to delete the entire List
 common_units.clear()
 print(common_units) #[]
+
+
+#Loop Through a List
+names = ['James','Alex','Ken','Stacy']
+for x, name in enumerate(names):
+    print(f'{x+1} {name}')
+
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+
+#Using a while Loop
+thislist = ["apple", "banana", "cherry"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+#LOOPING USING STRING COMPREHENSION
+thislist = ["apple", "banana", "cherry"]
+[print(x) for x in thislist]
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+print(newlist) #['apple', 'banana', 'mango']
+
+#An easy way to do the same 
+#newlist = [expression for item in iterable if condition == True]
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+print(newlist) #['apple', 'banana', 'mango']
+
+newlist = [x for x in fruits if x != "apple"]
+print(newlist) #['banana', 'cherry', 'kiwi', 'mango']
+
+# Set the values in the new list to upper case:
+newlist = [x.upper() for x in fruits]
+print(newlist) #['APPLE', 'BANANA', 'CHERRY', 'KIWI', 'MANGO']
+newlist = ['hello' for x in fruits]
+print(newlist) #['hello', 'hello', 'hello', 'hello', 'hello']
+newlist = [x if x != "banana" else "orange" for x in fruits]
+print(newlist) #['apple', 'orange', 'cherry', 'kiwi', 'mango']
